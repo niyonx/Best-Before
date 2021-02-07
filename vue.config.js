5dda961d128522e759d20dfe2b36e95678165778
niyonx <kenkenysy@gmail.com>
2021-02-07 12:21:36 -0500

    heroku fix

--- vue.config.js
+++ vue.config.js
@@ -20,9 +20,9 @@ module.exports = {
   },
   outputDir: 'dist',
   assetsDir: 'static',
-  // baseUrl: IS_PRODUCTION
-  // ? 'http://cdn123.com'
-  // : '/',
+  baseUrl: IS_PRODUCTION
+  ? 'https://bestbefore.herokuapp.com/'
+  : '/',
   // For Production, replace set baseUrl to CDN
   // And set the CDN origin to `yourdomain.com/static`
   // Whitenoise will serve once to CDN which will then cache
