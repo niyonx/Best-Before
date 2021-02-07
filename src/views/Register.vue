9a7374a5a661dd2626e964f7ff8c69f43c9f8e77
chelsieng <chelsiengmk@gmail.com>
2021-02-07 09:06:21 -0500

    Register polish

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -2,30 +2,15 @@
     <div class="row justify-content-center">
         <div class="col-lg-5 col-md-7">
             <div class="card bg-secondary shadow border-0">
-                <!-- <div class="card-header bg-transparent pb-5">
-                    <div class="text-muted text-center mt-2 mb-3">
-                        <small>Sign up with</small>
-                    </div>
-                    <div class="btn-wrapper text-center">
-                        <a href="#" class="btn btn-neutral btn-icon">
-                            <span class="btn-inner--icon"><img src="img/icons/common/github.svg"></span>
-                            <span class="btn-inner--text">Github</span>
-                        </a>
-                        <a href="#" class="btn btn-neutral btn-icon">
-                            <span class="btn-inner--icon"><img src="img/icons/common/google.svg"></span>
-                            <span class="btn-inner--text">Google</span>
-                        </a>
-                    </div>
-                </div> -->
+
                 <div class="card-body px-lg-5 py-lg-5">
                     <div class="text-center text-muted mb-4">
-                        <small>Sign up with credentials</small>
+                        <p class="display-4">Sign up with credentials</p>
                     </div>
                     <form role="form">
-
                         <base-input class="input-group-alternative mb-3"
                                     placeholder="Username"
-                                    addon-left-icon="ni ni-hat-3"
+                                    addon-left-icon="fa fa-user"
                                     v-model="model.username"
                                     >
                         </base-input>
@@ -44,23 +29,11 @@
                         </base-input>
 
                          <base-input class="input-group-alternative"
-                                    placeholder="Phone Number"
+                                    placeholder="Email"
                                     type="text"
-                                    addon-left-icon="ni ni-single-02"
+                                    addon-left-icon="fa fa-envelope"
                                     v-model="model.phone">
                         </base-input>
-
-                        <div class="text-muted font-italic">
-                            <small>password strength: <span class="text-success font-weight-700">strong</span></small>
-                        </div>
-
-                        <div class="row my-4">
-                            <div class="col-12">
-                                <base-checkbox class="custom-control-alternative">
-                                    <span class="text-muted">I agree with the <a href="#!">Privacy Policy</a></span>
-                                </base-checkbox>
-                            </div>
-                        </div>
                         <div class="text-center">
                             <base-button type="primary" class="my-4"
                                     @click="createUser(model.username, model.password, model.phone)"
@@ -72,9 +45,6 @@
             </div>
             <div class="row mt-3">
                 <div class="col-6">
-                    <a href="#" class="text-light">
-                        <small>Forgot password?</small>
-                    </a>
                 </div>
                 <div class="col-6 text-right">
                     <router-link to="/login" class="text-light">
