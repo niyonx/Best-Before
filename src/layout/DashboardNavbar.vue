28b08bd5ca2ea6c890d425fb9dd1237aee67376d
chelsieng <chelsiengmk@gmail.com>
2021-02-07 04:50:18 -0500

    remove search field

--- src/layout/DashboardNavbar.vue
+++ src/layout/DashboardNavbar.vue
@@ -3,16 +3,7 @@
               id="navbar-main"
               :show-toggle-button="false"
               expand>
-        <form class="navbar-search navbar-search-dark form-inline mr-3 d-none d-md-flex ml-lg-auto">
-            <div class="form-group mb-0">
-                <base-input placeholder="Search"
-                            class="input-group-alternative"
-                            alternative=""
-                            addon-right-icon="fas fa-search">
-                </base-input>
-            </div>
-        </form>
-        <ul class="navbar-nav align-items-center d-none d-md-flex">
+        <ul class="navbar-nav align-items-center mr-3 d-none d-md-flex ml-lg-auto">
             <li class="nav-item dropdown">
                 <base-dropdown class="nav-link pr-0">
                     <div class="media align-items-center" slot="title">
